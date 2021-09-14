a647e36c84df31fb8faaed84b7b07c3005dd86bb
Juvenal Yosa <juvenal.yosa@gmail.com>
2021-09-14 17:29:39 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,16 @@
+###### Modify the following parameter according with the guppy instalation#########
+
+#guppy_basecaller
+basecaller='/home/juvenal/ont-guppy/bin/guppy_basecaller'
+#guppy_barcoder
+barcoder='/home/juvenal/ont-guppy/bin/guppy_barcoder'
+#high-accuracy 
+guppy_cfg_hac='/home/juvenal/ont-guppy/data/dna_r9.4.1_450bps_hac.cfg'
+#fast mode basecalling
+guppy_cfg_fast='/home/juvenal/ont-guppy/data/dna_r9.4.1_450bps_fast.cfg'
+#emailme
+envia='bioestadisticausb@gmail.com'
+#pass de la cuenta de quien env'ia
+passw='b10s1l1c0'
+#El que recibe
+recibe='juvenal.yosa@gmail.com'
---
+++ emailme.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from datetime import datetime
+import config
+
+
+def emailme():
+    now = datetime.now() # current date and time
+    date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+    mail_content = "\nHola el Protocolo artic-ncov2019 ha finalizado en " + date_time + "\n\n que tengas un excelente d'ia!!!!\n\nPowered by El_Dryosa"
+
+    sender_address = config.envia
+    sender_pass = config.passw
+    receiver_address = config.recibe
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Artic protocolo ha finalizado'   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('un eMail a la cuenta {} fue enviado!'.format(receiver_address))
