b85e76b84236427be23c1033448f104180236028
Juvenal Yosa <42684155+jyosa@users.noreply.github.com>
2021-09-14 23:51:36 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ guppy_cfg_hac='/home/juvenal/ont-guppy/data/dna_r9.4.1_450bps_hac.cfg'
 #fast mode basecalling
 guppy_cfg_fast='/home/juvenal/ont-guppy/data/dna_r9.4.1_450bps_fast.cfg'
 #emailme
-envia='bioestadisticausb@gmail.com'
+envia='XXXXXXXXXX@gmail.com'
 #pass de la cuenta de quien env'ia
-passw='b10s1l1c0'
+passw='XXXXXXXX'
 #El que recibe
-recibe='juvenal.yosa@gmail.com'
+recibe='XXXXXXXX@gmail.com'
